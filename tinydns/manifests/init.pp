5d0e6fcd1e5ac65f3b16e2afd237c33b264002e9
root <root@tinybox.nws.oregonstate.edu>
2011-12-01 21:38:50 +0000

    added dependancy for services (install packages first).

--- tinydns/manifests/init.pp
+++ tinydns/manifests/init.pp
@@ -1,4 +1,6 @@
 class tinydns {
+	# Install all util packages before we start to setup all the services.
+	Class['tinydns::utils'] -> Class['tinydns::daemontools']
 	# Do work.
 	class { 'tinydns::setup':
 		subnet => "10.0.2",
